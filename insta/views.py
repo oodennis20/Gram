eb1635ecacc2d3d21c8dc582ebb97488c0e79da5
clyde <oodennis20@gmail.com>
2019-12-22 17:43:51 +0300

    changed settings to accomodate home template

--- insta/views.py
+++ insta/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http  import HttpResponse,Http404,HttpResponseRedirect
+from .models import *
 
 # Create your views here.
+def home(request):
+    images = Image.objects.all()
+    return render(request,"home.html",{"images":images})
