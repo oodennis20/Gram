8f73b33c90cba83ffc5fa520a140d850d4ae3a51
clyde <oodennis20@gmail.com>
2019-12-22 18:05:46 +0300

    configured database

--- insta/models.py
+++ insta/models.py
@@ -1,3 +1,34 @@
 from django.db import models
-
+from django.contrib.auth.models import User
+from tinymce.models import HTMLField
 # Create your models here.
+class Profile(models.Model):
+    profile_photo= models.ImageField(upload_to='picha/')
+    bio= HTMLField()
+    name = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def all_profiles(self):
+        return Profile.objects.all()
+
+class Image(models.Model):
+    insta_image = models.ImageField(upload_to='picha/')
+    caption = models.CharField(max_length=70)
+    profile= models.ForeignKey(Profile)
+    likes= models.IntegerField(default=0)
+    comments= HTMLField()
+
+    @classmethod
+    def all_images(self):
+
+        return Image.objects.all()
+
+
+
+
+
+
+
+
+
+
+
--- insta/views.py
+++ insta/views.py
@@ -6,3 +6,23 @@ from .models import *
 def home(request):
     images = Image.objects.all()
     return render(request,"home.html",{"images":images})
+
+def search_results(request):
+
+    if 'image' in request.GET and request.GET['image']:
+        search_images = request.GET.get("image")
+        searched_images = Image.search_by_category(search_images)
+        message = f"{search_images}"
+
+        return render(request, 'search.html',{"message":message,"images": searched_images})
+
+    else:
+        message = "You haven't searched for any image"
+        return render(request, 'search.html',{"message":message})
+
+def get_image_by_id(request,image_id):
+    try:
+        image = Image.objects.get(id = image_id)
+    except DoesNotExist:
+        raise Http404()
+    return render(request,"image.html", {"image":image})
