4868a47c489c4b17f3e7d347cd95f6cf043ddee5
clyde <oodennis20@gmail.com>
2019-12-28 16:16:27 +0300

    changed models

--- images/models.py
+++ images/models.py
@@ -1,11 +1,12 @@
 from django.db import models
 from django.contrib.auth.models import User
-from tinymce.models import HTMLField
+# from tinymce.models import HTMLField
 # Create your models here.
 class Profile(models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE, null=True)
     profile_photo= models.ImageField(upload_to='snap/', null=True)
     bio= models.CharField(max_length=240, null=True)
-    user = models.ForeignKey(User,on_delete=models.CASCADE, null=True)
+    
 
     def __str__(self):
         return self.name
@@ -21,6 +22,20 @@ class Profile(models.Model):
     class Meta:
         ordering = ['user']
 
+class Image(models.Model):
+    posted_by = models.ForeignKey(User, null=True)
+    insta_image = models.ImageField(upload_to='snap/', null=True)
+    caption = models.TextField(null=True)
+   
+    @classmethod
+    def all_images(self):
+        all_images = cls.objects.all()
+        return Image.objects.all()
+    
+    @classmethod
+    def get_user_images(cls, profile_id):
+        images=Image.objects.filter(profile_id=user.id)
+
 class Comment(models.Model):
     comment = models.CharField(max_length=70, blank=True)
     user = models.ForeignKey(User,on_delete=models.CASCADE, null=True)
@@ -32,26 +47,10 @@ class Comment(models.Model):
     def __str__(self):
         return self.comment
         
-class Image(models.Model):
-    insta_image = models.ImageField(upload_to='snap/', null=True)
-    caption = models.CharField(max_length=70, null=True)
-    like=models.IntegerField(default=0)
-    user = models.ForeignKey(User,on_delete=models.CASCADE, related_name='posts', null=True)
-    thoughts = models.ForeignKey(Comment,on_delete=models.CASCADE, null=True)
-    profile= models.ForeignKey(Profile, null=True)
-
-    @classmethod
-    def all_images(self):
-
-        return Image.objects.all()
-    
-    @classmethod
-    def get_user_images(cls, profile_id):
-        images=Image.objects.filter(profile_id=user.id)
         
 class Likes(models.Model):
-    likes= models.IntegerField (default=0)
-    liker = models.CharField(max_length=20)
+    image= models.ForeignKey(Image, related_name='likes', null=True)
+    liked_by = models.ForeignKey(User, related_name='liked_images', null=True)
 
         
 
